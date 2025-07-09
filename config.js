5f9c5c4019c9ee96eb3e6a2e0e37b111b946adaf
CypherX <chiderasamson11@gmail.com>
2025-07-09 21:35:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ global.autobio = true; // auto update bio
 global.chataudio = false; // Enable audio replies
 global.chatbot = false; // Enable text chatbot
 global.emojiReact = false; // Default: enabled
-global.ownernomer = "263786115435";
-global.ownername = "𝚚𝚞𝚎𝚎𝚗-𝚛𝚞𝚟𝚊 𝙰𝙸";
-global.ownernumber = '263786115435';
+global.ownernomer = "2347049797667";
+global.ownername = "Evill👿😈👹";
+global.ownernumber = '2347049797667';
 // Add country codes to block (e.g., +1, +91, +44)
 global.blockedCountryCodes = ["+92", "91", "+62"]; // Blocks US & UK numbers
 
